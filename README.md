4cf38c2de6faa035b0533ae447a4c5fd8aef3eca
Shevchik <Shev4ik.den@gmail.com>
2018-03-15 14:39:36 +0300

    Support multiple IO threads under epoll. Cleanup some things.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # UdpServerSocketChannel:
 
-UDP ServerSocketChannel for https://github.com/netty/netty
+UDP server for netty 4.1 which allocates user channel per remote address (just like netty TCP server) <br>
+By default allocated channels are never closed, so it's up to user to close channels upon read timeout or any other event <br>
+
+Can also use multiple threads for IO if running epoll capable system (via SO_REUSEPORT) <br>
 
 # Jenkins:
 
@@ -12,27 +15,26 @@ GNU LGPLv3
 
 # Usage example:
 
-```java
-import java.nio.charset.StandardCharsets;
+```import java.nio.charset.StandardCharsets;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFactory;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.DefaultEventLoopGroup;
+import io.netty.channel.ServerChannel;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.handler.timeout.ReadTimeoutHandler;
-import udpserversocketchannel.channel.NioUdpServerChannel;
+import udpserversocketchannel.channel.UdpServerChannel;
 
 public class ExampleUdpServer {
 
 	public static void main(String[] args) {
 		ServerBootstrap bootstrap = new ServerBootstrap()
-		.group(new NioEventLoopGroup(), new DefaultEventLoopGroup())
-		.channel(NioUdpServerChannel.class)
+		.group(new DefaultEventLoopGroup())
 		.childHandler(new ChannelInitializer<Channel>() {
 			@Override
 			protected void initChannel(Channel channel) throws Exception {
@@ -41,6 +43,17 @@ public class ExampleUdpServer {
 				.addLast(new Echo());
 			}
 		});
+		if (args.length > 0) {
+			int ioThreads = Integer.parseInt(args[0]);
+			bootstrap.channelFactory(new ChannelFactory<ServerChannel>() {
+				@Override
+				public ServerChannel newChannel() {
+					return new UdpServerChannel(ioThreads);
+				}
+			});
+		} else {
+			bootstrap.channel(UdpServerChannel.class);
+		}
 		bootstrap.bind("0.0.0.0", 1122).syncUninterruptibly();
 	}
 
@@ -61,10 +74,9 @@ public class ExampleUdpServer {
 		}
 		@Override
 		protected void channelRead0(ChannelHandlerContext ctx, ByteBuf bytebuf) throws Exception {
-			System.err.print(bytebuf.toString(StandardCharsets.UTF_8));
+			System.err.print("DATA: "+bytebuf.toString(StandardCharsets.UTF_8));
 			ctx.channel().writeAndFlush(Unpooled.copiedBuffer(bytebuf));
 		}
 	}
 
-}
-```
+}```
--- src/udpserversocketchannel/channel/UdpChannel.java
+++ src/udpserversocketchannel/channel/UdpChannel.java
@@ -3,6 +3,7 @@ package udpserversocketchannel.channel;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.AbstractChannel;
@@ -11,9 +12,7 @@ import io.netty.channel.ChannelMetadata;
 import io.netty.channel.ChannelOutboundBuffer;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.DefaultChannelConfig;
-import io.netty.channel.DefaultEventLoop;
 import io.netty.channel.EventLoop;
-import io.netty.channel.socket.DatagramPacket;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.internal.RecyclableArrayList;
 
@@ -21,13 +20,20 @@ public class UdpChannel extends AbstractChannel {
 
 	protected final ChannelMetadata metadata = new ChannelMetadata(false);
 	protected final DefaultChannelConfig config = new DefaultChannelConfig(this);
-	protected final NioUdpServerChannel serverchannel;
+
+	protected final UdpServerChannel serverChannel;
 	protected final InetSocketAddress remote;
 
-	protected UdpChannel(NioUdpServerChannel serverchannel, InetSocketAddress remote) {
+	protected UdpChannel(UdpServerChannel serverchannel, InetSocketAddress remote) {
 		super(serverchannel);
-		this.serverchannel = serverchannel;
+		this.serverChannel = serverchannel;
 		this.remote = remote;
+	}
+
+	protected AtomicBoolean isNew = new AtomicBoolean(true);
+
+	protected boolean getIsNew() {
+		return isNew.compareAndSet(true, false);
 	}
 
 	@Override
@@ -40,7 +46,7 @@ public class UdpChannel extends AbstractChannel {
 		return config;
 	}
 
-	private volatile boolean open = true;
+	protected volatile boolean open = true;
 
 	@Override
 	public boolean isActive() {
@@ -55,7 +61,7 @@ public class UdpChannel extends AbstractChannel {
 	@Override
 	protected void doClose() throws Exception {
 		open = false;
-		serverchannel.removeChannel(this);
+		serverChannel.doUserChannelRemove(this);
 	}
 
 	@Override
@@ -63,17 +69,16 @@ public class UdpChannel extends AbstractChannel {
 		doClose();
 	}
 
-	private final ConcurrentLinkedQueue<ByteBuf> buffers = new ConcurrentLinkedQueue<>();
+	protected final ConcurrentLinkedQueue<ByteBuf> buffers = new ConcurrentLinkedQueue<>();
 
 	protected void addBuffer(ByteBuf buffer) {
-		buffers.add(buffer);
+		this.buffers.add(buffer);
 	}
 
-	private boolean reading = false;
+	protected boolean reading = false;
 
 	@Override
 	protected void doBeginRead() throws Exception {
-		//is reading check, because the pipeline head context will call read again
 		if (reading) {
 			return;
 		}
@@ -91,7 +96,6 @@ public class UdpChannel extends AbstractChannel {
 
 	@Override
 	protected void doWrite(ChannelOutboundBuffer buffer) throws Exception {
-		//transfer all messages that are ready to be written to list
 		final RecyclableArrayList list = RecyclableArrayList.newInstance();
 		boolean freeList = true;
 		try {
@@ -109,35 +113,27 @@ public class UdpChannel extends AbstractChannel {
 				list.recycle();
 			}
 		}
-		//schedule a task that will write those entries
-		serverchannel.eventLoop().execute(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					for (Object buf : list) {
-						serverchannel.unsafe().write(new DatagramPacket((ByteBuf) buf, remote), voidPromise());
-					}
-					serverchannel.unsafe().flush();
-				} finally {
-					list.recycle();
-				}
-			}
-		});
+		serverChannel.doWrite(list, remote);
 	}
 
 	@Override
 	protected boolean isCompatible(EventLoop eventloop) {
-		return eventloop instanceof DefaultEventLoop;
+		return true;
 	}
 
 	@Override
 	protected AbstractUnsafe newUnsafe() {
-		return new UdpChannelUnsafe();
+		return new AbstractUnsafe() {
+			@Override
+			public void connect(SocketAddress addr1, SocketAddress addr2, ChannelPromise pr) {
+				throw new UnsupportedOperationException();
+			}
+		};
 	}
 
 	@Override
 	protected SocketAddress localAddress0() {
-		return serverchannel.localAddress0();
+		return serverChannel.localAddress0();
 	}
 
 	@Override
@@ -148,15 +144,6 @@ public class UdpChannel extends AbstractChannel {
 	@Override
 	protected void doBind(SocketAddress addr) throws Exception {
 		throw new UnsupportedOperationException();
-	}
-
-	private class UdpChannelUnsafe extends AbstractUnsafe {
-
-		@Override
-		public void connect(SocketAddress addr1, SocketAddress addr2, ChannelPromise pr) {
-			throw new UnsupportedOperationException();
-		}
-
 	}
 
 }
